3a5c13a5f46ee831728c93367277410ecdb79a26
Daniel Mahal <danielmahal@gmail.com>
2018-01-12 17:33:53 +0100

    Render

--- src/index.js
+++ src/index.js
@@ -1,19 +1,51 @@
+import createRegl from 'regl'
+import { fromPoll, combine } from 'kefir'
+import triangle from './triangle'
 import mouse from './mouse'
+import raf from './raf'
 
-const context = new window.AudioContext()
-const oscillator = context.createOscillator()
-const panner = context.createStereoPanner()
+const regl = createRegl()
+const drawTriangle = triangle(regl)
 
-oscillator.frequency.setTargetAtTime(120, 0, 0)
-oscillator.connect(panner)
-oscillator.start()
+const randomColor = () => [Math.random(), Math.random(), Math.random(), 1]
+const randomColorInterval = fromPoll(1000, randomColor).toProperty(randomColor)
 
-panner.connect(context.destination)
+const render = combine([
+  mouse,
+  randomColorInterval
+])
 
-mouse.onValue(({ x, y }) => {
-  oscillator.frequency.setTargetAtTime(220 - 120 * y, 0, 0.02)
-  panner.pan.setTargetAtTime(x * 2 - 1, 0, 0.02)
+mouse.onValue(value => {
+  console.log('mouse move:', value)
 })
+
+raf(render).onValue(([mouse, color]) => {
+  regl.poll()
+  regl.clear({ color: [0, 0, 0, 1] })
+
+  drawTriangle({
+    color: color,
+    position: mouse
+  })
+})
+
+// import mouse from './mouse'
+//
+// const context = new window.AudioContext()
+// const oscillator = context.createOscillator()
+// const panner = context.createStereoPanner()
+//
+// oscillator.frequency.setTargetAtTime(120, 0, 0)
+// oscillator.connect(panner)
+// oscillator.start()
+//
+// panner.connect(context.destination)
+//
+// mouse.onValue(({ x, y }) => {
+//   oscillator.frequency.setTargetAtTime(220 - 120 * y, 0, 0.02)
+//   panner.pan.setTargetAtTime(x * 2 - 1, 0, 0.02)
+// })
+
 //
 //
 // map(mouse.x, oscillator.a.frequency, {
--- src/mouse.js
+++ src/mouse.js
@@ -3,4 +3,7 @@ import { fromEvents } from 'kefir'
 export default fromEvents(window, 'mousemove', e => ({
   x: e.clientX / window.innerWidth,
   y: e.clientY / window.innerHeight
+})).toProperty(() => ({
+  x: 0.5,
+  y: 0.5
 }))
