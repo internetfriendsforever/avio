794585497b2973548ce32f4f54938fc77544116b
Daniel Mahal <danielmahal@gmail.com>
2018-01-22 20:43:53 +0100

    Tweak output

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import output from './output'
 const input1 = input.create()
 const audioOutput1 = output.audio.create()
 const circle = output.visual.circle.create().size(0.1)
+const triangle = output.visual.triangle.create().size(0.5).alpha(0.5).red(0)
 
-output.visual.triangle.create().size(0.5).alpha(0.5).green(0).blue(0)
-
+input1.tap.connect(triangle.blue).connect(triangle.green)
 input1.tap.multiply(input1.mouse.y).connect(circle.y).pow(2).connect(audioOutput1.gain).multiply(0.1).connect(circle.size)
 
 const note = input1.mouse.x.multiply(8).floor()
